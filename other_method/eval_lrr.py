bb6d51b3fa4739d6862bf7fe59caa9ad92206122
fjc <fujiachen2005@gmail.com>
2025-07-16 18:39:13 +0800

    update lrr_npr & fix baselines

--- other_method/eval_lrr.py
+++ other_method/eval_lrr.py
@@ -12,16 +12,15 @@ from typing import List, Tuple, Dict, Union
 from dataset import CustomDataset
 from metrics import AUROC, AUPR, MCC, Balanced_Accuracy, TPR_at_FPR5
 import torch.nn as nn
-import torch.nn.functional as F
+import copy
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--scoring_model_name", type=str, default="Qwen2-0.5B")
 parser.add_argument("--cache_dir", type=str, default="./model/")
-parser.add_argument("--method", type=str, default="likelihood", choices=["likelihood", "rank", "logrank", "entropy"])
 parser.add_argument('--eval_data_path', type=str, default='./data/MIRAGE_BENCH/DIG/rewrite.json', help='The path to the evaluation data. Default: ./data/DIG/rewrite.json.')
 parser.add_argument('--eval_data_format', type=str, default='MIRAGE', help='The format of the evaluation data. Should be MIRAGE or ImBD. Default: MIRAGE.')
-parser.add_argument('--save_path', type=str, default='./results/FastDetectGPT')
-parser.add_argument('--save_file', type=str, default='eval_baselines.json')
+parser.add_argument('--save_path', type=str, default='./results/LRR')
+parser.add_argument('--save_file', type=str, default='eval_lrr.json')
 parser.add_argument('--eval_batch_size', type=int, default=1, help='The batch size for evaluation. Default: 1.')
 parser.add_argument('--seed', type=int, default=42)
 
@@ -38,35 +37,16 @@ def get_likelihood(logits, labels, attention_mask=None):
         log_likelihood = log_likelihood.mean(dim=-1)
     return log_likelihood
 
-def get_rank(logits, labels, attention_mask=None):
-    """
-    different from the original code, because argsort in the original code is memory-intensive.
-    """
-    correct_token_logits = logits.gather(dim=-1, index=labels.unsqueeze(-1))
-    # Get ranks by counting how many logits are greater than the logit of the correct token.
-    # Add 1 for 1-based ranking.
-    # This avoids creating a huge sorted index tensor, which is memory-intensive.
-    ranks = (logits > correct_token_logits).sum(dim=-1) + 1
-    ranks = ranks.float()
-    if attention_mask is not None:
-        mask = attention_mask[:, 1:].float()
-        mask = mask[:, :ranks.shape[1]]
-        ranks = (ranks * mask).sum(dim=-1) / (mask.sum(dim=-1) + 1e-8)
-    else:
-        ranks = ranks.mean(dim=-1)
-    return ranks
-
 def get_logrank(logits, labels, attention_mask=None):
-    """
-    different from the original code, because argsort in the original code is memory-intensive.
-    """
     # Get the logits of the correct labels.
     # correct_token_logits will have shape (batch_size, seq_len, 1)
     correct_token_logits = logits.gather(dim=-1, index=labels.unsqueeze(-1))
+
     # Get ranks by counting how many logits are greater than the logit of the correct token.
     # Add 1 for 1-based ranking.
     # This avoids creating a huge sorted index tensor, which is memory-intensive.
     ranks = (logits > correct_token_logits).sum(dim=-1) + 1
+    
     ranks = ranks.float()
     log_ranks = torch.log(ranks)
     if attention_mask is not None:
@@ -76,17 +56,6 @@ def get_logrank(logits, labels, attention_mask=None):
     else:
         log_ranks = log_ranks.mean(dim=-1)
     return log_ranks
-
-def get_entropy(logits, labels, attention_mask=None):
-    entropy = F.softmax(logits, dim=-1) * F.log_softmax(logits, dim=-1)
-    entropy = -entropy.sum(-1)
-    if attention_mask is not None:
-        mask = attention_mask[:, 1:].float()
-        mask = mask[:, :entropy.shape[1]]
-        entropy = (entropy * mask).sum(dim=-1) / (mask.sum(dim=-1) + 1e-8)
-    else:
-        entropy = entropy.mean(dim=-1)
-    return entropy
 
 def from_pretrained(cls, model_name, kwargs, cache_dir):
     # use local model if it exists
@@ -100,78 +69,53 @@ def from_pretrained(cls, model_name, kwargs, cache_dir):
     return cls.from_pretrained(model_name, **kwargs, cache_dir=cache_dir, device_map='auto')
 
 
-methods = {
-    'likelihood': get_likelihood,
-    'rank': get_rank,
-    'logrank': get_logrank,
-    'entropy': get_entropy,
-}
-
-
-class Baselines(nn.Module):
+class LRR(nn.Module):
     def __init__(self,
-                 scoring_model_name: str=None,
-                 scoring_model: AutoModelForCausalLM=None,
-                 scoring_tokenizer: AutoTokenizer=None,
-                 cache_dir: str=None,
-                 method: callable=get_rank,
-                 ):
+                 scoring_model_name,
+                 cache_dir):
         super().__init__()
-        self.cache_dir = cache_dir
-        if scoring_model_name is not None:
-            self.scoring_model_name = scoring_model_name
-            self.scoring_model = from_pretrained(AutoModelForCausalLM,
+        self.scoring_model_name = scoring_model_name
+        self.scoring_model = from_pretrained(AutoModelForCausalLM,
+                                             scoring_model_name,
+                                             cache_dir=cache_dir,
+                                             kwargs=dict(torch_dtype=torch.float16))
+        self.scoring_tokenizer = from_pretrained(AutoTokenizer,
                                                  scoring_model_name,
-                                                 cache_dir=cache_dir,
-                                                 kwargs=dict(torch_dtype=torch.float16))
-            self.scoring_tokenizer = from_pretrained(AutoTokenizer,
-                                                     scoring_model_name,
-                                                     kwargs={'padding_side': 'right',
-                                                             'use_fast': True if 'facebook/opt-' not in scoring_model_name else False},
-                                                     cache_dir=cache_dir,)
-        else:
-            assert scoring_model is not None and scoring_tokenizer is not None, "You should provide scoring_model_name or scoring_model and scoring_tokenizer."
-            self.scoring_model = scoring_model
-            self.scoring_tokenizer = scoring_tokenizer
-            self.scoring_model_name = scoring_model.config._name_or_path
-        
-        self.method = method
+                                                 kwargs={'padding_side': 'right',
+                                                         'use_fast': True if 'facebook/opt-' not in scoring_model_name else False},
+                                                 cache_dir=cache_dir,)
+
+        self.cache_dir = cache_dir
         if self.scoring_tokenizer.pad_token is None:
             self.scoring_tokenizer.pad_token = self.scoring_tokenizer.eos_token
+            self.scoring_tokenizer.pad_token_id = self.scoring_tokenizer.eos_token_id
 
-    def forward(self, input_text):
-        if isinstance(input_text, str):
-            input_texts = [input_text]
-        else:
-            input_texts = input_text
-
-        # original text
-        tokenized = self.scoring_tokenizer(input_texts, return_tensors="pt", padding=True, return_token_type_ids=False)
-        tokenized = {k: v.to(self.scoring_model.device) for k, v in tokenized.items()}
-        labels = tokenized['input_ids'][:, 1:]
-        attention_mask = tokenized['attention_mask'] if 'attention_mask' in tokenized else None
+    def forward(self, input_texts):
         with torch.no_grad():
+            tokenized = self.scoring_tokenizer(input_texts, return_tensors="pt", padding=True, return_token_type_ids=False)
+            tokenized = {k: v.to(self.scoring_model.device) for k, v in tokenized.items()}
+            labels = tokenized['input_ids'][:, 1:]
+            attention_mask = tokenized['attention_mask'] if 'attention_mask' in tokenized else None
             logits = self.scoring_model(**tokenized).logits[:, :-1]
-            scores = self.method(logits, labels, attention_mask=attention_mask)
-
+            likelihood = get_likelihood(logits, labels, attention_mask)
+            logrank = get_logrank(logits, labels, attention_mask)
+            scores = - likelihood / logrank
         return scores.tolist()
-
 
 if __name__ == "__main__":
     args = parser.parse_args()
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
     accelerator = accelerate.Accelerator()
-    model = Baselines(scoring_model_name=args.scoring_model_name,
-                      cache_dir=args.cache_dir,
-                      method=methods[args.method])
+    model = LRR(scoring_model_name=args.scoring_model_name,
+                cache_dir=args.cache_dir)
     model.eval()
     dataset = CustomDataset(data_path=args.eval_data_path, data_format=args.eval_data_format)
     local_original_scores = []
     local_rewritten_scores = []
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=args.eval_batch_size, shuffle=False, collate_fn=dataset.collate_fn)
     model, data_loader = accelerator.prepare(model, data_loader)
-    for idx, item in tqdm(enumerate(data_loader), total=len(data_loader), desc=f"Evaluating {args.method} on {args.eval_data_path.split('/')[-1]}"):
+    for idx, item in tqdm(enumerate(data_loader), total=len(data_loader), desc=f"Evaluating LRR on {args.eval_data_path.split('/')[-1]}"):
         local_original_scores.extend(model(item['original']))
         local_rewritten_scores.extend(model(item['rewritten']))
     accelerator.wait_for_everyone()
@@ -199,7 +143,7 @@ if __name__ == "__main__":
         print(f'Eval MCC: {best_mcc:.4f} | Eval Balanced Accuracy: {best_balanced_accuracy:.4f}')
 
         result_dict = {
-            'method': args.method,
+            'method': 'LRR',
             'scoring_model_name': args.scoring_model_name,
             'eval_dataset': args.eval_data_path.split("/")[-1].split(".json")[0],
             'eval_batch_size': args.eval_batch_size,
