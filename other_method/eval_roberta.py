e8a67faef552d28049d659affd62032c0dd6ce44
fjc <fujiachen2005@gmail.com>
2025-07-17 18:42:50 +0800

    fix some code

--- other_method/eval_roberta.py
+++ other_method/eval_roberta.py
@@ -1,8 +1,4 @@
-# Copyright (c) Guangsheng Bao.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
+# Revise from https://github.com/baoguangsheng/fast-detect-gpt
 import os
 import numpy as np
 import torch
